a06e49f0f18916c2105a142c397556c3df88401f
singerliu226 <singer226@163.com>
2025-03-02 01:22:13 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from langchain.memory import ConversationBufferMemory
 
 
 def get_chat_response(prompt, memory, api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=api_key)
+    model = ChatOpenAI(model = "gpt-3.5-turbo",openai_api_key=api_key)
     chain = ConversationChain(llm=model, memory=memory)
 
     response = chain.invoke({"input": prompt})
@@ -14,6 +14,7 @@ def get_chat_response(prompt, memory, api_key):
     return response["response"]
 
 
-memory = ConversationBufferMemory(return_messages=True)
-print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
-print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
+#memory = ConversationBufferMemory(return_messages=True)
+#print(get_chat_response("牛顿提出过哪些知名的定律？", memory, os.getenv("OPENAI_API_KEY")))
+#print(get_chat_response("我上一个问题是什么？", memory, os.getenv("OPENAI_API_KEY")))
+
